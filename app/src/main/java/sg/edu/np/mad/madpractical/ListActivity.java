6b533d705bce1131426b752340d34c9ea04e796b
Bing En <115916417+bingenk@users.noreply.github.com>
2023-05-28 00:19:19 +0800

    Add files via upload

---
+++ app/src/main/java/sg/edu/np/mad/madpractical/ListActivity.java
@@ -0,0 +1,51 @@
+package sg.edu.np.mad.madpractical;
+
+import androidx.appcompat.app.AlertDialog;
+import androidx.appcompat.app.AppCompatActivity;
+import android.content.DialogInterface;
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+import java.util.Random;
+
+public class ListActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_list);
+
+        ImageView image = findViewById(R.id.imageView5);
+
+        image.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(ListActivity.this);
+                builder.setTitle("Profile");
+                builder.setMessage("MADness");
+                builder.setCancelable(true);
+                builder.setPositiveButton("View", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Random r = new Random();
+                        int randomNumber = r.nextInt(1000000000);
+                        Intent activity = new Intent(ListActivity.this, MainActivity.class);
+                        activity.putExtra("Key", String.valueOf(randomNumber));
+                        startActivity(activity);
+                    }
+                });
+
+                builder.setNegativeButton("Close", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+
+                    }
+                });
+
+                AlertDialog alert = builder.create();
+                alert.show();
+            }
+        });
+    }
+}
